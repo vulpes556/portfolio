6b72973ebadb0f786cfcd0771d0cfcb1f41c281e
Bálint <szelasszie@gmail.com>
2025-03-31 15:06:08 +0200

    basic bio component added

--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
+import Bio from "@/components/Bio/Bio";
 
 
 export default function Home() {
   return (
     <div className="main">
-      <h1>secton1</h1>
-      <h1>secton2</h1>
-      <h1>secton3</h1>
-      <h1>secton4</h1>
-      <h1>secton5</h1>
+      <div className="left-section">
+        <Bio />
+      </div>
+      <div className="right-section">
+        <h1>secton2</h1>
+        <h1>secton3</h1>
+        <h1>secton4</h1>
+        <h1>secton5</h1>
+      </div>
     </div>
   );
 }
---
+++ components/Bio/Bio.jsx
@@ -0,0 +1,36 @@
+import "./Bio.css"
+import { FaMapLocation } from "react-icons/fa6";
+import Image from "next/image"
+
+export default function Bio() {
+
+    function calculateAge(birthDate) {
+        const today = new Date();
+        const birth = new Date(birthDate);
+
+        let age = today.getFullYear() - birth.getFullYear();
+        let month = today.getMonth() - birth.getMonth();
+
+        if (month < 0 || (month === 0 && today.getDate() < birth.getDate())) {
+            age--;
+        }
+
+        return age;
+    }
+
+
+
+    return (
+        <div className="bio-main">
+            <div className="portrait-container">
+            <Image className="portrait" alt="An image of the developer" layout="intrinsic" width={200} height={200} src={"/portrait2.jpg"}>
+            </Image>
+            </div>
+            <div className="name-div">Bálint Kovács ({calculateAge("1996-07-22")}) </div>
+            <span>Full Stack Developer</span>
+            <div className="residence-div">
+                <FaMapLocation /> Miskolc, Hungary
+            </div>
+        </div>
+    )
+}
