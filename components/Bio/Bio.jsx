20018f0ffa3dcde6a0442ca42865950354ac7dc7
Bálint <szelasszie@gmail.com>
2025-04-05 10:24:38 +0200

    updated the "bio" component

--- components/Bio/Bio.jsx
+++ components/Bio/Bio.jsx
@@ -4,7 +4,7 @@ import { FaMapLocation } from "react-icons/fa6";
 import Image from "next/image"
 import { CiLinkedin } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
-
+import { MdEmail } from "react-icons/md";
 
 
 export default function Bio() {
@@ -24,7 +24,6 @@ export default function Bio() {
     }
 
 
-
     return (
         <div id="bio" className="bio-main">
             <div className="portrait-container">
@@ -36,6 +35,9 @@ export default function Bio() {
             <div className="residence-div">
                 <FaMapLocation /> Miskolc, Hungary
             </div>
+            <div className="email-div">
+                <MdEmail /> balint.kovacs.dev@gmail.com
+            </div>
             <div className="contacts">
                 <button onClick={() => window.open("https://www.linkedin.com/in/b%C3%A1lint-kov%C3%A1cs-821bb2327/")} >
                     <CiLinkedin size={45} />
@@ -44,6 +46,17 @@ export default function Bio() {
                     <FaGithub size={45} />
                 </button>
             </div>
+            <div className="self-description">
+                <p>Hi all! My name is Bálint, and I'm a Junior Fullstack Developer.</p>
+                <p>
+                    Over the past year, I've been diving into both trending backend and frontend technologies,
+                    but I’m definitely more backend-oriented.
+                </p>
+                <p>
+                    My goal is to continuously improve, learn new technologies, and deliver high-quality code.
+                </p>
+            </div>
+
         </div>
     )
 }
