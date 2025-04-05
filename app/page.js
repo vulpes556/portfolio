caf2483e912c8ee460a9517b6107c8e56f71d34d
Bálint <szelasszie@gmail.com>
2025-04-05 10:25:04 +0200

    reorganized the main layout

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Bio from "@/components/Bio/Bio";
+import Contact from "@/components/Contact/Contact";
 import Projects from "@/components/Projects/Projects";
 import TechStack from "@/components/TechStack/TechStack";
 
@@ -6,12 +7,17 @@ import TechStack from "@/components/TechStack/TechStack";
 export default function Home() {
   return (
     <div className="main">
+      <div className="top-section">
       <div className="left-section">
         <Bio />
       </div>
       <div className="right-section">
         <TechStack />
         <Projects />
+      </div>
+      </div>
+      <div className="bottom-section">
+      <Contact />
       </div>
     </div>
   );
